54da9857ce83c143c6ace927a0fbcb3ad0a660d6
tanaphonlnwjud <tanaphon.thok@ku.th>
2026-03-13 11:39:32 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -37,8 +37,8 @@ app.get('/testdb-new', async (req, res) => {
     try {
         const results = await conn.query('SELECT * FROM users');
         res.json(results[0]);
-    } catch (err) {
-        console.error('Error connecting to the database:', err);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
@@ -49,32 +49,68 @@ app.get('/users', async (req, res) => {
     res.json(results[0]);
 })
 
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstname) {
+        errors.push("กรุณากรอกชื่อ");
+    }
+    if (!userData.lastname) {
+        errors.push("กรุณากรอกนามสกุล");
+    }
+    if (!userData.age) {
+        errors.push("กรุณากรอกอายุ");
+    }
+    if (!userData.gender) {
+        errors.push("กรุณาเลือกเพศ");
+    }
+    if (!userData.interests) {
+        errors.push("กรุณาเลือกงานอดิเรก");
+    }
+    if (!userData.description) {
+        errors.push("กรุณากรอกคำอธิบาย");
+    }
+    return errors;
+}
+
 // path: = POST /users สำหรับเพิ่ม user ใหม่
 app.post('/users', async (req, res) => {
     try{
         let user = req.body;
+        const errors =validateData(user);
+        if(errors.length > 0){
+            throw {
+                message: "กรุณากรอกข้อมูลให้ครบถ้วน",
+                errors: errors
+            }
+        }
+
         const results = await conn.query('INSERT INTO users SET ?', user);
+        console.log('results:',results);
         res.json({
             message: "User added successfully",
             data: results[0]
         });
-    }catch(err){
-        console.error('Error inserting user:', err);
-        res.status(500).json({ error: 'Error adding user' });
+    }catch(error){
+        const errorMessage = error.message || 'Error adding user';
+        const errors = error.errors || [];
+        console.error('Error inserting user:', error);
+        res.status(500).json({
+            message: errorMessage,
+            errors: errors
+        });
     }
 })
-
 // path: = GET /user/:id สำหรับดึงข้อมูล user ตาม id
- app.get('/users/:id', async (req, res) => {
+app.get('/users/:id', async (req, res) => {
     try {
         let id = req.params.id;
         const results = await conn.query('SELECT * FROM users WHERE id = ?', id);
-        if(results[0].length === 0){
+        if (results[0].length === 0) {
             throw { statusCode: 404, message: 'User not found' };
         }
         res.json(results[0][0]);
-    }catch(err){
-        console.error('Error fetching user:', err);
+    } catch (error) {
+        console.error('Error fetching user:', error);
         let statusCode = err.statusCode || 500;
         res.status(statusCode).json({
             error: err.message || 'Error fetching user'
@@ -84,31 +120,31 @@ app.post('/users', async (req, res) => {
 
 //path: = PUT /users/:id สำหรับแก้ไขข้อมูล user ตาม id
 app.put('/users/:id', async (req, res) => {
-    try{
+    try {
         let id = req.params.id;
         let updatedUser = req.body;
         const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updatedUser, id]);
         res.json({
-        message: "User updated successfully",
-        data: results[0]
-         });
-    }catch(err){
-        console.error('Error updating user:', err);
+            message: "User updated successfully",
+            data: results[0]
+        });
+    } catch (error) {
+        console.error('Error updating user:', error);
         res.status(500).json({ error: 'Error updating user' });
     }
 })
 
 //path: = DELETE /users/:id สำหรับลบ user ตาม id
-app.delete ('/users/:id', async (req, res) => {
-    try{
+app.delete('/users/:id', async (req, res) => {
+    try {
         let id = req.params.id;
         const results = await conn.query('DELETE FROM users WHERE id = ?', id);
         res.json({
             message: "User deleted successfully",
             data: results[0]
         });
-    }catch(err){
-        console.error('Error deleting user:', err);
+    } catch (error) {
+        console.error('Error deleting user:', error);
         res.status(500).json({ error: 'Error deleting user' });
     }
 })
